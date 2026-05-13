e6d2798091e8fa866e9b83c7920ec74533086fb5
Jose Maria Vazquez <jomavazquez@yahoo.es>
2026-05-13 08:46:26 +0200

    Vite updated

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2015',
+    rollupOptions: {
+      output: {
+        format: 'iife',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
 })
